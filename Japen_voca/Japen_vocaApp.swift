f97a12b34fe8562887b2fc1fb87e38a6b3a43553
Ahnjaehyun <wogus3673@naver.com>
2024-02-14 22:42:28 +0900

    Initial Commit

---
+++ Japen_voca/Japen_vocaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japen_vocaApp.swift
+//  Japen_voca
+//
+//  Created by 안재현 on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Japen_vocaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
