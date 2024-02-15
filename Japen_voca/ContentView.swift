686270865d448a3bc2608a04f4a75007f1bd4e47
Ahnjaehyun <wogus3673@naver.com>
2024-02-15 14:04:45 +0900

    update 2

--- Japen_voca/ContentView.swift
+++ Japen_voca/ContentView.swift
@@ -9,16 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Button("back"){
+                
+            }
+                .font(.title)
+            Button("back"){
+                
+            }
+                .font(.title)
+            Button("back"){
+                
+            }
+                .font(.title)
+            Button("back"){
+                
+            }
+                .font(.title)
+            Button("back"){
+                
+            }
+                .font(.title)
         }
         .padding()
+        
+        
+        
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+
